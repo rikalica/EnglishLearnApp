3e61ec38bac92e5d0d60051cf177189c538cf8ec
rikalica <hatajimui@yahoo.co.jp>
2019-05-26 22:32:39 +0900

    Initial commit

---
+++ app/src/main/java/android/wings/websarva/englishlearnapp/EnglishQuiz.java
@@ -0,0 +1,13 @@
+package android.wings.websarva.englishlearnapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnglishQuiz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_english_quiz);
+    }
+}
